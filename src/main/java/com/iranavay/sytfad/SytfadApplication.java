cb00a22f7552be68a24778e1a77e07d01b50a1d7
Anıl DEMİR <anil.demir@botas.gov.tr>
2023-09-01 13:55:25 +0300

    Initial Commit

---
+++ src/main/java/com/iranavay/sytfad/SytfadApplication.java
@@ -0,0 +1,13 @@
+package com.iranavay.sytfad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SytfadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SytfadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iranavay/sytfad/SytfadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iranavay.sytfad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SytfadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
